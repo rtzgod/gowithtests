315664e855a0bc56d6eb805b42f76c433a4342cc
rtzgod <azimaidynuly1@gmail.com>
2025-08-20 12:25:52 +0500

    iterator

--- mocking/main.go
+++ mocking/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
@@ -29,9 +30,19 @@ type Sleeper interface {
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
+	for i := range countDownFrom(countdownStart) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
+}
+
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
 }

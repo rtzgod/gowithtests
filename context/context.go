633cd96da6ac13a15950324f1b8a000673124da8
rtzgod <azimaidynuly1@gmail.com>
2025-08-26 19:57:36 +0500

    context

---
+++ context/context.go
@@ -0,0 +1,16 @@
+package context
+
+import (
+	"context"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
